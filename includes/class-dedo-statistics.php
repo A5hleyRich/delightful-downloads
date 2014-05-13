6e0e1403af63299340bd15cbe4a68629a86c3411
Ashley Rich <hello@ashleyrich.com>
2014-05-13 15:18:56 +0100

    Added setup table function.

---
+++ includes/class-dedo-statistics.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Statistics Class
+ *
+ * @package  	Delightful Downloads
+ * @author   	Ashley Rich
+ * @copyright   Copyright (c) 2014, Ashley Rich
+ * @since    	1.4
+*/
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+class DEDO_Statistics {
+
+	/**
+	 * The custom database table name.
+	 *
+	 * @access public
+	 * @since 1.4
+	 */
+	public $table = 'ddownload_statistics';
+
+	/**
+	 *	Init statistics class.
+	 *
+	 * @access public
+	 * @since 1.4
+	 * @return void
+	 */
+	public function __construct() {
+
+		global $wpdb;
+
+		// Add custom table to wpdb.
+		$wpdb->ddownload_statistics = $wpdb->prefix . $this->table;
+
+	}
+
+	/**
+	 * Setup custom database table structure.
+	 *
+	 * @access public
+	 * @since 1.4
+	 * @return void
+	 */
+	public function setup_table() {
+
+		global $wpdb;
+
+		$sql = "
+			CREATE TABLE $wpdb->ddownload_statistics (
+				ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+				status varchar(7) NOT NULL DEFAULT 'success',
+				date datetime NOT NULL,
+				post_id bigint(20) unsigned NOT NULL,
+				user_id bigint(20) unsigned NOT NULL DEFAULT '0',
+				user_ip varbinary(16) NOT NULL,
+				user_agent varchar(255) NOT NULL,
+			PRIMARY KEY  (ID),
+			KEY status (status),
+			KEY date (date),
+			KEY post_id (post_id),
+			KEY user_ip (user_ip)
+			) DEFAULT CHARSET=$wpdb->charset;
+		";
+
+		// Include our database function and run
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		dbDelta( $sql );
+	}
+
+}
+
+// Initiate the logging system
+$GLOBALS['dedo_statistics'] = new DEDO_Statistics();
