2d2f06c4f1ed1f285ff554289813897f39d5969d
Ashley Rich <hello@ashleyrich.com>
2014-06-12 13:19:08 +0100

    Dashboard design completed.

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -47,14 +47,39 @@ function dedo_dashboard_downloads_widget() {
 				<span><?php _e( 'Last 30 Days', 'delightful-downloads' ); ?></span>
 			</li>
 			<li>
-				<strong>759,567</strong>
+				<strong>1,759,567</strong>
 				<span><?php _e( 'All Time', 'delightful-downloads' ); ?></span>
 			</li>
 		</ul>
 	</div>
-
 	<div id="ddownload-popular">
-		
+		<h4><?php _e( 'Popular Downloads', 'delightful-downloads' ); ?></h4>
+		<ol>
+			<li>
+				<a href="#">Dolor Fermentum Malesuada<span class="count">1,896</span></a>
+			</li>
+			<li>
+				<a href="#">Dolor Fermentum Malesuada<span class="count">1,156</span></a>
+			</li>
+			<li>
+				<a href="#">Dolor Fermentum Malesuada<span class="count">1,001</span></a>
+			</li>
+			<li>
+				<a href="#">Dolor Fermentum Malesuada<span class="count">863</span></a>
+			</li>
+			<li>
+				<a href="#">Dolor Fermentum Malesuada<span class="count">501</span></a>
+			</li>
+		</ol>
+		<div class="sub">	
+			<a href="<?php echo admin_url( 'edit.php?post_type=dedo_download&page=dedo_statistics' ); ?>"><?php _e( 'View Statistics', 'delightful-downloads' ); ?></a>
+			<select>
+				<option><?php _e( 'Last 24 Hours', 'delightful-downloads' ); ?></option>
+				<option><?php _e( 'Last 7 Days', 'delightful-downloads' ); ?></option>
+				<option><?php _e( 'Last 30 Days', 'delightful-downloads' ); ?></option>
+				<option><?php _e( 'All Time', 'delightful-downloads' ); ?></option>
+			</select>
+		</div>
 	</div>
 	
 	<?php
