dccc04cedb08c8055a00acd331e508c128d587f4
Ashley Rich <hello@ashleyrich.com>
2014-08-21 22:31:27 +0100

    Removed old shortcode generator. Added jQuery Chosen.

--- includes/admin/media-button.php
+++ includes/admin/media-button.php
@@ -17,7 +17,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
  */
 function dedo_media_button( $context ) {
 	
-	return $context . '<a href="#" id="dedo-media-button" class="button add-download" data-editor="content" title="Add Download"><span class="wp-media-buttons-icon"></span>Add Download</a>';	
+	return $context . '<a href="#dedo-shortcode-modal" id="dedo-media-button" class="button dedo-modal-action add-download" data-editor="content" title="Add Download"><span class="wp-media-buttons-icon"></span>Add Download</a>';	
 }
 add_filter( 'media_buttons_context', 'dedo_media_button' );
 
@@ -27,97 +27,26 @@ add_filter( 'media_buttons_context', 'dedo_media_button' );
  * @since  1.0
  */
 function dedo_media_modal() {
-	global $dedo_options;
-	
-	$downloads = new WP_Query( 'post_type=dedo_download&nopaging=true&orderby=title&order=ASC' );
-	?>
-	<div id="dedo-download-modal" style="display: none">
-		<div class="media-modal">
-			<a id="dedo-download-modal-close" class="media-modal-close" href="#" title="Close"><span class="media-modal-icon"></span></a>
-			<div class="media-modal-content">
-				<div class="media-frame-title">
-					<h1><?php _e( 'Insert Download', 'delightful-downloads' ); ?></h1>
-				</div>
-				<div class="left-panel">
-					<div class="dedo-download-toolbar">
-						<input type="search" id="dedo-download-search" class="search" placeholder="<?php _e( 'Search', 'delightful-downloads' ); ?>" />
-					</div>
-					<div class="dedo-download-list">
-						<table class="wp-list-table widefat" cellspacing="0">
-							<thead>
-								<tr>
-									<th scope="col" class="title_column"><?php _e( 'Title', 'delightful-downloads' ); ?></th>
-									<th scope="col" class="size_column"><?php _e( 'Size', 'delightful-downloads' ); ?></th>
-									<th scope="col" class="count_column"><?php _e( 'Downloads', 'delightful-downloads' ); ?></th>
-								</tr>
-							</thead>
+	global $pagenow;
 
-							<tbody id="the-list">
-<?php
-	while ( $downloads->have_posts() ) {
-		$downloads->the_post();
-		$download_id = get_the_ID();
-		$file_size = get_post_meta( $download_id, '_dedo_file_size', true );
-		$download_count = get_post_meta( $download_id, '_dedo_file_count', true );
-
-?>
-								<tr id="dedo-download-<?php echo esc_attr( $download_id ); ?>" data-id="<?php echo esc_attr( $download_id ); ?>" data-size="<?php echo esc_attr( ( !$file_size ) ? '--' : size_format( $file_size, 1 ) ); ?>" data-count="<?php echo esc_attr( ( !$download_count ) ? '0' : number_format_i18n( $download_count, 0 ) ); ?>">
-									<td class="title_column"><strong><?php the_title(); ?></strong></td>			
-									<td class="size_column"><?php echo esc_attr( ( !$file_size ) ? '--' : size_format( $file_size, 1 ) ); ?></td>
-									<td class="count_column"><?php echo esc_attr( ( !$download_count ) ? '0' : number_format_i18n( $download_count, 0 ) ); ?></td>
-								</tr>
-<?php
-	}
-?>	
+	// Only run in post/page creation and edit screens
+	if ( in_array( $pagenow, array( 'post.php', 'page.php', 'post-new.php', 'post-edit.php' ) ) ) { 
+		?>
 			
-							</tbody>
-						</table>
-					</div>
-				</div>
-				<div class="right-panel">
-					<div class="download-details" style="display: none">
-						<h3><?php _e( 'Download Details', 'delightful-downloads' ); ?></h3>
-						<div class="meta">
-							<div class="title"><strong></strong></div>
-							<div class="count"><?php _e( 'Downloads:', 'delightful-downloads' ); ?> <span></span></div>
-							<div class="size"></div>
-						</div>
-						<label for="dedo-download-text"><?php _e( 'Text', 'delightful-downloads' ); ?>:</label>
-						<input type="text" name="dedo-download-text" id="dedo-download-text" placeholder="Default" />
-						<label for="dedo-download-style"><?php _e( 'Style', 'delightful-downloads' ); ?>:</label>
-						<select name="dedo-download-style" id="dedo-download-style">
-							<option value="dedo_default"><?php _e( 'Default', 'delightful-downloads' ); ?></option>
-							<?php
-							$styles = dedo_get_shortcode_styles();
-							
-							foreach ( $styles as $key => $value ) {
-								echo '<option value="' . $key . '">' . $value['name'] . '</option>';	
-							}
-							?>
+			<div id="dedo-shortcode-modal" class="dedo-modal" style="display: block; width: 40%; left: 50%; margin-left: -20%;">
+				<a href="#" class="dedo-modal-close" title="Close"><span class="media-modal-icon"></span></a>
+				<div class="dedo-modal-content">
+					<h1><?php _e( 'Insert Download', 'delightful-downloads' ); ?></h1>
+					
+					<p>
+						<select id="dedo-select-download-dropdown">
+							<option>Test</option>
 						</select>
-						<div class="dedo-download-color-container" style="display: none">
-							<label for="dedo-download-color"><?php _e( 'Color', 'delightful-downloads' ); ?>:</label>
-							<select name="dedo-download-color" id="dedo-download-color">
-								<option value="dedo_default"><?php _e( 'Default', 'delightful-downloads' ); ?></option>
-								<?php
-								$colors = dedo_get_shortcode_buttons();
-
-								foreach ( $colors as $key => $value ) {
-									echo '<option value="' . $key . '">' . $value['name'] . '</option>';	
-								}
-								?>
-							</select>
-						</div>
-						<a href="#" id="dedo-download-button" class="button-primary"><?php _e( 'Insert Download', 'delightful-downloads' ); ?></a>
-						<a href="#" id="dedo-filesize-button" class="button"><?php _e( 'Insert File Size', 'delightful-downloads' ); ?></a>
-						<a href="#" id="dedo-count-button" class="button"><?php _e( 'Insert Download Count', 'delightful-downloads' ); ?></a>
-					</div>
+					</p>
 				</div>
-						
 			</div>
-		</div>
-		<div class="media-modal-backdrop"></div>
-	</div>
-	<?php
+
+		<?php 
+	}
 }
 add_action( 'admin_footer', 'dedo_media_modal', 100 );
--- includes/scripts.php
+++ includes/scripts.php
@@ -37,23 +37,30 @@ function dedo_admin_enqueue_scripts( $page ) {
 	// Register scripts
 	wp_register_script( 'dedo-admin-js-global', DEDO_PLUGIN_URL . 'assets/js/dedo-admin-global.js', array( 'jquery' ), DEDO_VERSION, true );
 	wp_register_script( 'dedo-admin-js-legacy-logs', DEDO_PLUGIN_URL . 'assets/js/dedo-admin-legacy-logs.js', array( 'jquery' ), DEDO_VERSION, true ); // 1.4 upgrade
-	wp_register_script( 'dedo-admin-js-media-button', DEDO_PLUGIN_URL . 'assets/js/dedo-admin-media-button.js', array( 'jquery', 'jquery-ui-selectable' ), DEDO_VERSION, true );
+	wp_register_script( 'dedo-admin-js-media-button', DEDO_PLUGIN_URL . 'assets/js/dedo-admin-media-button.js', array( 'jquery', 'dedo-jqueryChosen' ), DEDO_VERSION, true );
 	wp_register_script( 'dedo-admin-js-post-download', DEDO_PLUGIN_URL . 'assets/js/dedo-admin-post-download.js', array( 'jquery', 'plupload-all', 'jqueryFileTree' ), DEDO_VERSION, true );
 	wp_register_script( 'jqueryFileTree', DEDO_PLUGIN_URL . 'assets/js/jqueryFileTree/jqueryFileTree.js', array( 'jquery' ), DEDO_VERSION, true );
+	wp_register_script( 'dedo-jqueryChosen', DEDO_PLUGIN_URL . 'assets/js/jqueryChosen/chosen.jquery.min.js', array( 'jquery' ), DEDO_VERSION, true );
 
 	// Register styles
 	wp_register_style( 'dedo-css-admin', DEDO_PLUGIN_URL . 'assets/css/delightful-downloads-admin.css', '', DEDO_VERSION, 'all' );
 	wp_register_style( 'jqueryFileTree-css', DEDO_PLUGIN_URL . 'assets/js/jqueryFileTree/jqueryFileTree.css', '', DEDO_VERSION, 'all' );
+	wp_register_style( 'dedo-jqueryChosen-css', DEDO_PLUGIN_URL . 'assets/js/jqueryChosen/chosen.min.css', '', DEDO_VERSION, 'all' );
 
 	// Enqueue on all admin pages
 	wp_enqueue_style( 'dedo-css-admin' );
 	wp_enqueue_script( 'dedo-admin-js-global' );
-	wp_enqueue_script( 'dedo-admin-js-media-button' );
 
 	// Enqueue on dedo_download post add/edit screen
-	if ( in_array( $page, array( 'post.php', 'post-new.php', 'post-edit.php' ) ) && get_post_type() == 'dedo_download' ) {
+	if ( in_array( $page, array( 'post.php', 'page.php', 'post-new.php', 'post-edit.php' ) ) && get_post_type() == 'dedo_download' ) {
 		wp_enqueue_script( 'dedo-admin-js-post-download' );
 		wp_enqueue_style( 'jqueryFileTree-css' );
+	}
+
+	// Enqueue on all post/edit screen
+	if ( in_array( $page, array( 'post.php', 'page.php', 'post-new.php', 'post-edit.php' ) ) ) {
+		wp_enqueue_script( 'dedo-admin-js-media-button' );
+		wp_enqueue_style( 'dedo-jqueryChosen-css' );
 	}
 }
 add_action( 'admin_enqueue_scripts', 'dedo_admin_enqueue_scripts' );
