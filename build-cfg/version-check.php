618c545d10d9691dcc6e2a141888b533050a8555
Ashley Rich <hello@ashleyrich.com>
2015-07-12 13:00:11 +0100

    Build config added

---
+++ build-cfg/version-check.php
@@ -0,0 +1,7 @@
+<?php
+$version_checks = array(
+	"delightful-downloads.php" => array(
+		'@Version:\s+(.*)\n@'                              => 'header',
+		'@define\(\s+\'DEDO_VERSION\',\s+\'(.*?)\'\s+\);@' => 'constant',
+	)
+);
