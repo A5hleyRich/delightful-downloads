97033404cc9af62e21e3baf6f43ed74513d38e71
Ashley Rich <hello@ashleyrich.com>
2015-11-28 17:45:58 +0000

    Deploying version 1.6

--- build-cfg/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$tmp_dir = $parent_dir . '/builds';
-$zip_dir = $tmp_dir;
--- build-cfg/version-check.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$version_checks = array(
-	'delightful-downloads.php' => array(
-		'@Version:\s+(.*)\n@'                              => 'header',
-		'@define\(\s+\'DEDO_VERSION\',\s+\'(.*?)\'\s+\);@' => 'constant',
-	),
-);
