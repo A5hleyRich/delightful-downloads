b62cac632c71c1b155e28c4a79da8c3c0fcd6e36
Ashley Rich <hello@ashleyrich.com>
2015-11-28 16:03:21 +0000

    Don't version check grunt or npm

--- build-cfg/version-check.php
+++ build-cfg/version-check.php
@@ -1,13 +1,8 @@
 <?php
+
 $version_checks = array(
 	'delightful-downloads.php' => array(
 		'@Version:\s+(.*)\n@'                              => 'header',
 		'@define\(\s+\'DEDO_VERSION\',\s+\'(.*?)\'\s+\);@' => 'constant',
-	),
-	'Gruntfile.js'             => array(
-		'@package_version:\s+\'(.*)\'@' => 'Gruntfile.js',
-	),
-	'package.json'             => array(
-		'@\"version\":\s+\"(.*)\",@' => 'package.json',
 	),
 );
